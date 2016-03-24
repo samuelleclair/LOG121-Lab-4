bfcf84448fa3cc61ae62ad0245455eead9667853
Grasseh <steve.gagnev4si@gmail.com>
2016-03-24 09:53:27 -0400

    Fixed Kev coding like shit

--- src/ViewManager/Controller/ImageCommand.java
+++ src/ViewManager/Controller/ImageCommand.java
@@ -1,9 +1,9 @@
 package ViewManager.Controller;
 
-import ViewManager.Controller.Controller;
+import ViewManager.Controller.Command;
 
 /**
  * Created by kevin on 2016-03-24.
  */
-public abstract class ImageCommand implements Controller.Command {
+public abstract class ImageCommand implements Command {
 }
