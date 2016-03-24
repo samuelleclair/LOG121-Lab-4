1d661f6ed6190b674b702874dd125e2d25e95b03
Grasseh <steve.gagnev4si@gmail.com>
2016-03-24 11:14:21 -0400

    Added Image in model

--- src/ViewManager/Model/ImageComplexe.java
+++ src/ViewManager/Model/ImageComplexe.java
@@ -1,7 +1,74 @@
 package ViewManager.Model;
 
+import ViewManager.Util.Observer.Observable;
+
 /**
- * Created by kevin on 2016-03-24.
+ * Represents a Data Model of an image, with it's display parameters
+ * @author Steve Gagne
  */
-public class ImageComplexe {
+public class ImageComplexe extends Observable {
+    TransParametre paramTranslation;
+    ZoomParametre paramZoom;
+    ImageConcrete image;
+    /**
+     * Creates a new complex image
+     * @param imagePath 
+     */
+    public ImageComplexe(String imagePath){
+        image = new ImageConcrete(imagePath);
+        this.notifyObservers();
+    }
+    
+    /**
+     * Sets the direction of this image's parameters
+     * @param x Translation in X
+     * @param y Translation in Y
+     */
+    public void setDirection(float x,float y){
+        paramZoom.setParamX(x);
+        paramZoom.setParamY(y);
+        this.notifyObservers();
+    }
+    
+     /**
+     * Sets the zoom of this image's parameters
+     * @param z Zoom magnification coefficient
+     */
+    public void setZoom(float z){
+        paramTranslation.setParam(z);
+        this.notifyObservers();
+    }
+    
+    /**
+     * Sets a new photo for this image
+     * @param imagePath The file path on disk
+     */
+    public void setImage(String imagePath){
+        image = new ImageConcrete(imagePath);
+        this.notifyObservers();
+    }
+    
+    /**
+     * Obtain the X translation factor
+     * @return X Translation Factor
+     */
+    public float getTranslationX(){
+        return this.paramTranslation.getParamX();
+    }
+    
+    /**
+     * Obtain the Y translation factor
+     * @return Y Translation Factor
+     */
+    public float getTranslationY(){
+        return this.paramTranslation.getParamY();
+    }
+    
+    /**
+     * Obtain the zoom factor
+     * @return Zoom Factor
+     */
+    public float getZoom(){
+        return this.paramZoom.getParam();
+    }
 }
--- src/ViewManager/Model/ImageConcrete.java
+++ src/ViewManager/Model/ImageConcrete.java
@@ -4,4 +4,8 @@ package ViewManager.Model;
  * Created by kevin on 2016-03-24.
  */
 public class ImageConcrete {
+    String imagePath;
+    public ImageConcrete(String _imagePath){
+        imagePath = _imagePath;
+    }
 }
