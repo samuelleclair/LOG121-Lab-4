c4a74fc59bd9d90cb4dba08e69ad7bc4674c979d
Grasseh <steve.gagnev4si@gmail.com>
2016-03-24 09:19:00 -0400

    Added base

---
+++ src/ViewManager/Application.java
@@ -0,0 +1,29 @@
+/*
+ * Cours : LOG 121
+ * Session : H2016
+ * Groupe : 02
+ * Projet : Laboratoire #3
+ * Etudiants : Kevin Beausejour
+ *             Steve Gagne
+ *             Samuel Leclair
+ * Codes Perm. : BEAK07129009
+ *               GAGS08109409
+ *               LECS06069305
+ *
+ */
+package ViewManager;
+
+/**
+ *
+ * @author stevegagne
+ */
+public class Application {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
